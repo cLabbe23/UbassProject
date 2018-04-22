ad28546b58bd928218a37e6f493218fe856a2035
cLabbe23 <carlos.labbe.pena@gmail.com>
2018-04-22 19:58:26 -0300

    Proyectil
    codigo experimental

---
+++ Proyectil.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Proyectil : MonoBehaviour {
+    public float rapidez = 1;
+    public int danio = 1;
+
+
+
+    public Proyectil(float rapidez)
+    {
+        this.rapidez = rapidez;
+    }
+    public int getDanio()
+    {
+        return this.danio;
+    }
+    public void SetDanio(int danio)
+    {
+        this.danio = danio;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        var choque = collision.gameObject;
+        var vida = choque.GetComponent<Vida>();
+        if (vida != null)
+        {
+            vida.recibeDanio(1);
+        }
+        Destroy(gameObject);
+    }
+
+    // Use this for initialization
+    void Start() {
+    
+   }
+// Update is called once per frame
+
+ void Update () {
+		
+	
+}
+}
---
+++ Vida.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Vida : MonoBehaviour {
+    public const int vidamaxima = 3;
+    public int vidaActual = vidamaxima;
+
+    public void recibeDanio(int danio)
+    {
+        vidaActual -= danio;
+        if(vidaActual <= 0)
+        {
+            vidaActual = 0;
+            Debug.Log("Mision finalizada.");
+        }
+    }
+    
+
+    
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
